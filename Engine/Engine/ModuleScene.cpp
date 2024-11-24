3d5c8b2bce82abeae0eb6fe96a1e860e7febcd44
guillemal <guillem.alqueza@estudiantat.upc.edu>
2024-11-24 23:59:14 +0100

    Quadtree

--- Engine/Engine/ModuleScene.cpp
+++ Engine/Engine/ModuleScene.cpp
@@ -3,6 +3,7 @@
 
 ModuleScene::ModuleScene(App* app) : Module(app), root(nullptr)
 {
+	sceneBounds = AABB(glm::vec3(-20, -20, -20), glm::vec3(20, 20, 20));
 }
 
 ModuleScene::~ModuleScene()
@@ -13,12 +14,18 @@ bool ModuleScene::Awake()
 {
 	root = CreateGameObject("Untitled Scene", nullptr);
 
+	sceneQuadtree = new Quadtree(sceneBounds);
+
 	return true;
 }
 
 bool ModuleScene::Update(float dt)
 {
+	UpdateQuadtree();
+
 	root->Update();
+
+	DrawQuadtree();
 
 	return true;
 }
@@ -37,4 +44,60 @@ GameObject* ModuleScene::CreateGameObject(const char* name, GameObject* parent)
 	if (parent != nullptr) parent->children.push_back(gameObject);
 
 	return gameObject;
+}
+
+void ModuleScene::UpdateQuadtree()
+{
+    delete sceneQuadtree;
+    sceneQuadtree = new Quadtree(sceneBounds);
+
+    AddGameObjectToQuadtree(root);
+}
+
+void ModuleScene::AddGameObjectToQuadtree(GameObject* gameObject)
+{
+    if (gameObject == nullptr)
+        return;
+
+    std::vector<Mesh*> meshes;
+	std::vector<glm::mat4> transforms;
+    CollectMeshes(gameObject, meshes, transforms);
+
+    for (size_t i = 0; i < meshes.size(); ++i)
+    {
+        if (meshes[i] != nullptr)
+        {
+            sceneQuadtree->AddObject(meshes[i], transforms[i]);
+        }
+    }
+}
+
+void ModuleScene::CollectMeshes(GameObject* gameObject, std::vector<Mesh*>& meshes, std::vector<glm::mat4>& transforms)
+{
+    if (gameObject == nullptr)
+        return;
+
+    for (auto* component : gameObject->components)
+    {
+        if (component->type == ComponentType::MESH)
+        {
+            ComponentMesh* meshComponent = static_cast<ComponentMesh*>(component);
+            if (meshComponent->mesh != nullptr)
+            {
+                meshes.push_back(meshComponent->mesh);
+				transforms.push_back(gameObject->transform->globalTransform);
+            }
+        }
+    }
+
+    for (auto* child : gameObject->children)
+    {
+        CollectMeshes(child, meshes, transforms);
+    }
+}
+
+void ModuleScene::DrawQuadtree()
+{
+    if (sceneQuadtree != nullptr)
+        sceneQuadtree->Draw();
 }
--- Engine/Engine/ModuleScene.h
+++ Engine/Engine/ModuleScene.h
@@ -2,6 +2,9 @@
 
 #include "Module.h"
 #include "GameObject.h"
+#include "Quadtree.h"
+#include <vector>
+#include <glm/glm.hpp>
 
 class GameObject;
 
@@ -17,6 +20,15 @@ public:
 
 	GameObject* CreateGameObject(const char* name, GameObject* parent);
 
+	void UpdateQuadtree();
+	void DrawQuadtree();
+
+private:
+	void AddGameObjectToQuadtree(GameObject* gameObject);
+	void CollectMeshes(GameObject* gameObject, std::vector<Mesh*>& meshes, std::vector<glm::mat4>& transforms);
+
 public:
 	GameObject* root = nullptr;
+	Quadtree* sceneQuadtree = nullptr;
+	AABB sceneBounds;
 };
